5697e6addd572a9cf24080579004d665c1fcbe4b
Leonidas3169 <ananb228@gmail.com>
2025-06-23 15:10:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("evetBtn").addEventListener("click", function() {
+    alert("Tebrikler, yetişkinsin!");
+});
+
+document.getElementById("hayirBtn").addEventListener("click", function() {
+    alert("Üzgünüm, bu içerik sana uygun değil.");
+});
