2d1e527037ce4c100c52e03d30c59b908975e63c
Leonidas <ananb228@gmail.com>
2026-02-07 16:04:13 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("evetBtn").addEventListener("click", function() {
-    alert("Tebrikler, yetişkinsin!");
-});
-
-document.getElementById("hayirBtn").addEventListener("click", function() {
-    alert("Üzgünüm, bu içerik sana uygun değil.");
-});
